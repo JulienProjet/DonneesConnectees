4e89c3d9ce98d45336173d3386de7946362bb1b5
JulienProjet <79588918+JulienProjet@users.noreply.github.com>
2023-03-15 14:31:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use((req, res, next)=>{
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'POST');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Headers', 'Authorization');
 	next();
 });
 
