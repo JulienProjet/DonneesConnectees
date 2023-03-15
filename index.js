a5d080ae76039ffc7fc6615fe573d3e7790c2d77
JulienProjet <79588918+JulienProjet@users.noreply.github.com>
2023-03-15 15:43:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ var app = express();
 app.use(express.static("static"));
 
 
-/* app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://julienprojet.github.io');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); //https://julienprojet.github.io
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
     next();
-}); */
+});
+/*
 app.use(cors())
 app.options('*', cors())
-
+*/
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
