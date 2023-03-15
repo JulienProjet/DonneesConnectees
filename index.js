ea0bb8d8926793cbe027fd66550f93277b045268
JulienProjet <79588918+JulienProjet@users.noreply.github.com>
2023-03-15 14:38:36 +0100

    update

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(bodyParser.json());
 app.use(express.static("static"));
 
 var cors = require('cors');
-app.use((req, res, next)=>{
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'POST');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	res.header('Access-Control-Allow-Headers', 'Authorization');
-	next();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
 });
 
 app.get('/',(req,res)=>{
